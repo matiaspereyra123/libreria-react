f90baa657cdbff5aa4bc0cddeffe61be3b572f9c
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2022-08-10 09:39:56 -0300

    Libreria con react

---
+++ src/components/LibrosApp.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+function LibrosApp() {
+  return (  <div id="wrapper">  
+    <main >
+
+     <SideBar/>  
+ 
+ 
+    <hr/>
+    </main> </div>
+  )
+
+  
+  
+}
+
+export default LibrosApp
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './assets/css/app.css';
 import reportWebVitals from './reportWebVitals';
+import LibrosApp from './components/LibrosApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+       <LibrosApp/>
+    
+   
   </React.StrictMode>
 );
 
